e0a355b656885a2b3dafa89f6720fc08e803cc9c
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-05-08 22:27:27 -0500

    Creating a pointer string

--- basic/3-strings.c
+++ basic/3-strings.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-	char c[] = "Hello";
+	char *c = malloc(sizeof(char) * 6);
+	strcpy(c, "Hello");
 	printf("%s\n", c);
+	free(c);
 }
