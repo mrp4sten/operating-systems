c8362223ceeb77068772f56c0a6c17358183164c
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-05-08 22:54:42 -0500

    Ways of create an struct

--- basic/5-structs.c
+++ basic/5-structs.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 
 typedef struct {
-	double x;
-	double y;
+  double x;
+  double y;
 } Point;
 
 int main() {
-	Point point = {0.5, 5.9};
-	printf("x: %f, y:%f\n", point.x, point.y);
+  Point first_point = {0.5, 5.9};
+  printf("x: %f, y:%f\n", first_point.x, first_point.y);
+
+  Point another_point = {.y = 7.5, .x = 5.3};
+  printf("x: %f, y:%f\n", another_point.x, another_point.y);
+
+  /* With pointer */
+  Point *point = malloc(sizeof(Point));
+  point->x = 2.3;
+  point->y = 3.5;
+  printf("x: %f, y:%f\n", point->x, point->y);
+
+  free(point);
 }
