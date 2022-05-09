5839c0a42f3d3da1e11017c64abcc23c4bd96ab3
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-05-08 22:36:32 -0500

    Creating a multidimensional array with pointer to chars pointers

--- basic/4-multidimensional-arrays.c
+++ basic/4-multidimensional-arrays.c
@@ -3,7 +3,16 @@
 #include <string.h>
 
 int main() {
-	char c[][8] = {"Hello", "Message"};
-	printf("%s\n", c[0]);
-	printf("%s\n", c[1]);
+	char **strings = malloc(sizeof(char*) * 2); 
+	char *msg1 = malloc(sizeof(char) * 8);
+	char *msg2 = malloc(sizeof(char) * 8);
+
+	strcpy(msg1, "Hello");
+	strcpy(msg2, "Message");
+
+	strings[0] = msg1;
+	strings[1] = msg2;
+
+	printf("%s\n", strings[0]);
+	printf("%s\n", strings[1]);
 }
