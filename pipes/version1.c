b670f6b65e5741788e81b6db3d4e0204e918089a
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-05-10 18:03:38 -0500

    Understanding pipes with strings

--- pipes/version1.c
+++ pipes/version1.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
   int pipe1[2];
   pipe(pipe1);
-  char c = 'X';
+  char s[64];
 
   if (fork()) {
     /* Father */
     close(pipe1[1]);
-    read(3, &c, sizeof(c));
+    read(pipe1[0], s, 64);
     close(pipe1[0]);
-    printf("Im Father I have write from descriptor %i character: %c\n",
-           pipe1[0], c);
+    printf("Im Father I have write from descriptor %i string: %s\n",
+           pipe1[0], s);
   } else {
     /* Children */
-    c = 'A';
+		strcpy(s, "Hello world, SO Course");
     close(pipe1[0]);
-    write(4, &c, sizeof(c));
+    write(pipe1[1], s, strlen(s));
     close(pipe1[1]);
-    printf("Im Children I have write into descriptor %i character: %c\n",
-           pipe1[1], c);
+    printf("Im Children I have write into descriptor %i string: %s\n",
+           pipe1[1], s);
   }
 }
