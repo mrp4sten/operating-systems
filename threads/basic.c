35284ca4c02c93ace29e2b1fd985de54eec0967b
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-05-10 21:43:40 -0500

    Multiple threads

--- threads/basic.c
+++ threads/basic.c
@@ -4,16 +4,18 @@
 
 void *thread_func(void *arg) {
 	int value_to_int = *(int*)arg;
-	printf("Im thread, value: %i\n", value_to_int);
+	printf("Value: %i\n", value_to_int);
 
 	pthread_exit(0);
 }
 
 int main() {
-	pthread_t thread;
-	int value = 5;
-	pthread_create(&thread, NULL, thread_func, &value);
+	pthread_t thread1, thread2;
+	int value1 = 5, value2 = 14;
+	pthread_create(&thread1, NULL, thread_func, &value1);
+	pthread_create(&thread2, NULL, thread_func, &value2);
 	printf("Main thread\n");
-	pthread_join(thread, NULL);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
 }
 
