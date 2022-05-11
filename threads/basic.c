4f9cdddbaefd1f8136d8b7f80b3a31e4fdd0b304
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-05-10 22:32:15 -0500

    Array adition with threads

--- threads/basic.c
+++ threads/basic.c
@@ -2,20 +2,61 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void *thread_func(void *arg) {
-	int value_to_int = *(int*)arg;
-	printf("Value: %i\n", value_to_int);
+#define SIZE 100
 
+int num_threads;
+int *array;
+int *thread_result;
+
+void *sum(void *arg) {
+	int id = *(int*)arg;
+	int nums_to_sum = SIZE / num_threads;
+	int start = nums_to_sum * id;
+	int end = start + nums_to_sum;
+
+	if (id == num_threads - 1) {
+		end = SIZE;
+	}
+
+	int sum = 0;
+	for (int i = start; i < end; i++) {
+		sum += array[i];
+	}
+	thread_result[id] = sum;
+	printf("Thread %i has result: %i\n", id, sum);
 	pthread_exit(0);
 }
 
-int main() {
-	pthread_t thread1, thread2;
-	int value1 = 5, value2 = 14;
-	pthread_create(&thread1, NULL, thread_func, &value1);
-	pthread_create(&thread2, NULL, thread_func, &value2);
-	printf("Main thread\n");
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-}
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("Give me theads number\n");
+		exit(-1);
+	}
+	num_threads = atoi(argv[1]);
+	array = malloc(sizeof(int) * SIZE);
+	thread_result = malloc(sizeof(int) * num_threads);
 
+	for (int i = 0; i < SIZE; i++) {
+		array[i] = i + 1;
+	}
+
+	pthread_t threads[num_threads];
+	int thread_ids[num_threads];
+
+	int total = 0;
+
+	for (int i = 0; i < num_threads; i++) {
+		thread_ids[i] = i;
+		pthread_create(&threads[i], NULL, sum, (void*)&thread_ids[i]);
+	}
+
+	for (int i = 0; i < num_threads; i++) {
+		pthread_join(threads[i], NULL);
+		total += thread_result[i];
+	}
+
+	printf("Total: %i\n", total);
+
+	free(array);
+	free(thread_result);
+}
